2d4f828dd00aec494aaa2d7fb8ebf09a71bdc00b
KhvanNikol <109013298+KhvanNikol@users.noreply.github.com>
2022-09-03 09:33:29 +0900

    added functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+function formateData(timestamp) {
+  let date = newDate(timestamp);
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let day = days[date.getDate()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${day}${hour}${minutes}`;
+}
+function weatherDescription(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.main.wind;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+dateElement.innerHTML = formateData();
 let apiKey = "8bd19818f1a9f1fdc34f62c973ba9f77";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(weatherDescription);
