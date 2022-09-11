8e491bc1a8bdc6354987c6acc676a4c726f5f00c
KhvanNikol <109013298+KhvanNikol@users.noreply.github.com>
2022-09-11 13:01:50 +0900

    deleted Fahrenheit

--- src/index.js
+++ src/index.js
@@ -88,25 +88,8 @@ function submitCity(event) {
   let cityElement = document.querySelector("#city-input");
   search(cityElement.value);
 }
-function displayFahrenheitTemp(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-let celciusTemperature = null;
-
-function displayCelciusTemp(event) {
-  event.preventDefault();
-  let celciusElement = document.querySelector("#temperature");
-  celciusElement.innerHTML = Math.round(celciusTemperature);
-}
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitCity);
-let fahrenheitElement = document.querySelector("#fahrenheit-link");
-fahrenheitElement.addEventListener("click", displayFahrenheitTemp);
-let celciusElement = document.querySelector("#celcius-link");
-celciusElement.addEventListener("click", displayCelciusTemp);
 
 search("Tokyo");
